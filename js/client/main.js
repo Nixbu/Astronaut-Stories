07c32bbb46a51ee7a4fed8e822e1d36e97d94a7f
Nixbu <83171543+Nixbu@users.noreply.github.com>
2024-12-15 15:52:58 +0200

    Added search by earth date functionality to retrieve all rover images in a certain date

--- js/client/main.js
+++ js/client/main.js
@@ -1,5 +1,6 @@
 const API_KEY = "lrufIrxkoorq8sriuuo3LdPU1Cnu1fnrIpr0kRm0";
-const url = `https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${API_KEY}`;
+const API_KEY_PARAMETER = `api_key=${API_KEY}`
+const ROVER_DATA_ENDPOINT = "https://api.nasa.gov/mars-photos/api/v1/rovers";
 
 // UI Module - Responsible for managing the DOM and events
 const UI = (function () {
@@ -30,13 +31,14 @@ const searchModule = (function () {
     // Fetch rover data from the API
     async function fetchRoverData() {
         try {
-            const response = await fetch(url);
+            const response = await fetch(`${ROVER_DATA_ENDPOINT}?${API_KEY_PARAMETER}`);
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             const data = await response.json();
             roverData = data; // Save the fetched data
-            roverNames = roverData.rovers.map(rover => rover.name);
+            roverNames = roverData?.rovers?.map(rover => rover.name);
+            console.log(roverNames);
             console.log(roverData); // Log the fetched data to verify
             return roverData;
         } catch (error) {
@@ -44,16 +46,32 @@ const searchModule = (function () {
         }
     }
 
-    // Search by Earth Date
-    async function searchByEarthDate() {
-        const inputValue = earthDateInput.value.trim();
-        if (inputValue) {
-            const filteredData = await fetch(url);
-            console.log(filteredData); // Show filtered data
+    async function searchByEarthDate(event) {
+        event.preventDefault();
+        const dateInputValue = earthDateInput.value.trim();
+        console.log(dateInputValue);
+        if (dateInputValue) {
+            try {
+                const promises = roverNames.map(roverName =>
+                    fetch(`${ROVER_DATA_ENDPOINT}/${roverName}/photos?${API_KEY_PARAMETER}&earth_date=${dateInputValue}`)
+                        .then(response => {
+                            if(!response.ok) {
+                                throw new Error(`HTTP error! Status: ${response.status}`);
+                            }
+                            return response.json()
+                        })
+                );
+                const searchResults = await Promise.all(promises); // Wait for all fetches to complete
+
+                console.log(searchResults); // Show the results
+            } catch (error) {
+                console.error("Error fetching rover data:", error);
+            }
         } else {
             console.log("Please enter a valid Earth Date.");
         }
     }
+
 
     return {
         fetchRoverData: fetchRoverData,
